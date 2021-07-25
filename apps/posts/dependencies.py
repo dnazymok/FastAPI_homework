fdd64bf6754e289f1f928c59cfd43fc66121d601
dnazymok <dnazymok@gmail.com>
2021-07-25 19:07:52 +0300

    fix session usage

--- apps/posts/dependencies.py
+++ apps/posts/dependencies.py
@@ -1,14 +1,23 @@
 import aiohttp
 
 from typing import Any, Dict, List
+
+from fastapi import Depends
+
 from .models import Post, CreatePostParams, DetailPost, UpdatePostParams
 
 
+async def get_session() -> aiohttp.ClientSession:
+    async with aiohttp.ClientSession() as session:
+        yield session
+
+
 class PostRepository:
-    def __init__(self):
+    def __init__(self, session):
         self._post_endpoint = "https://jsonplaceholder.typicode.com/posts"
         self._author_endpoint = "https://jsonplaceholder.typicode.com/users/"
         self._comment_endpoint = "https://jsonplaceholder.typicode.com/comments?postId="
+        self._session = session
 
     async def list_posts(self) -> List[Post]:
         raw_posts = await self._list_posts()
@@ -27,41 +36,40 @@ class PostRepository:
         return raw_post
 
     async def _list_posts(self):
-        async with aiohttp.ClientSession() as session:
-            post_resp = await session.get(self._post_endpoint)
-            author_resp = await session.get(self._author_endpoint)
-            raw_posts = await post_resp.json()
-            raw_authors = await author_resp.json()
-            await self._match_authors_and_posts(raw_posts, raw_authors)
-            return raw_posts
+        post_resp = await self._session.get(self._post_endpoint)
+        author_resp = await self._session.get(self._author_endpoint)
+        raw_posts = await post_resp.json()
+        raw_authors = await author_resp.json()
+        await self._match_authors_and_posts(raw_posts, raw_authors)
+        return raw_posts
 
     async def _create_post(self, post: CreatePostParams) -> Dict[str, Any]:
-        async with aiohttp.ClientSession() as session:
-            post_resp = await session.post(self._post_endpoint, json=post.dict())
-            author_resp = await session.get(self._author_endpoint)
-            raw_post = await post_resp.json()
-            raw_authors = await author_resp.json()
-            raw_post["author"] = await self._get_author_by_id(raw_post["userId"], raw_authors)
-            return raw_post
+        post_resp = await self._session.post(self._post_endpoint,
+                                             json=post.dict())
+        author_resp = await self._session.get(self._author_endpoint)
+        raw_post = await post_resp.json()
+        raw_authors = await author_resp.json()
+        raw_post["author"] = await self._get_author_by_id(
+            raw_post["userId"], raw_authors)
+        return raw_post
 
     async def _detail_post(self, id_: int) -> Dict[str, Any]:
-        async with aiohttp.ClientSession() as session:
-            post_resp = await session.get(self._post_endpoint + f"/{id_}")
-            raw_post = await post_resp.json()
-            raw_post["comments"] = await self._get_comments(id_)
-            raw_post["author"] = await self._get_author(raw_post["userId"])
-            return raw_post
+        post_resp = await self._session.get(self._post_endpoint + f"/{id_}")
+        raw_post = await post_resp.json()
+        raw_post["comments"] = await self._get_comments(id_)
+        raw_post["author"] = await self._get_author(raw_post["userId"])
+        return raw_post
 
-    async def _update_post(self, post: UpdatePostParams, id_: int) -> Dict[str, Any]:
-        async with aiohttp.ClientSession() as session:
-            post_resp = await session.patch(self._post_endpoint + f"/{id_}", json=post.dict())
-            raw_post = await post_resp.json()
-            return raw_post
+    async def _update_post(self, post: UpdatePostParams, id_: int) -> Dict[
+        str, Any]:
+        post_resp = await self._session.patch(self._post_endpoint + f"/{id_}",
+                                              json=post.dict())
+        raw_post = await post_resp.json()
+        return raw_post
 
     async def _get_author(self, id_: int) -> Dict[str, Any]:
-        async with aiohttp.ClientSession() as session:
-            resp = await session.get(self._author_endpoint + f"/{id_}")
-            return await resp.json()
+        resp = await self._session.get(self._author_endpoint + f"/{id_}")
+        return await resp.json()
 
     async def _get_author_by_id(self, id_: int, raw_authors) -> Dict[str, Any]:
         for author in raw_authors:
@@ -69,10 +77,9 @@ class PostRepository:
                 return author
 
     async def _get_comments(self, post_id: int):
-        async with aiohttp.ClientSession() as session:
-            resp = await session.get(self._comment_endpoint + str(post_id))
-            comments = await resp.json()
-            return comments
+        resp = await self._session.get(self._comment_endpoint + str(post_id))
+        comments = await resp.json()
+        return comments
 
     async def _match_authors_and_posts(self, raw_posts, raw_authors):
         for post in raw_posts:
@@ -85,13 +92,9 @@ class PostRepository:
 
 
 class PostRepositoryFactory:
-    def __init__(self):
-        self._repo = None
-
-    def __call__(self):
-        if self._repo is None:
-            self._repo = PostRepository()
-        return self._repo
+    async def __call__(self, session: aiohttp.ClientSession = Depends(
+        get_session)) -> PostRepository:
+        return PostRepository(session=session)
 
 
 get_post_repository = PostRepositoryFactory()
--- apps/users/dependencies.py
+++ apps/users/dependencies.py
@@ -1,12 +1,16 @@
 import aiohttp
 
 from typing import Any, Dict, List
+
+from fastapi import Depends
+
 from .models import User, CreateUserParams
 
 
 class UserRepository:
-    def __init__(self):
+    def __init__(self, session):
         self._endpoint = "https://jsonplaceholder.typicode.com/users"
+        self._session = session
 
     async def list_users(self) -> List[User]:
         raw_users = await self._list_users()
@@ -17,29 +21,28 @@ class UserRepository:
         return self._convert_user(raw_user)
 
     async def _list_users(self):
-        async with aiohttp.ClientSession() as session:
-            resp = await session.get(self._endpoint)
-            raw_users = await resp.json()
-            return raw_users
+        resp = await self._session.get(self._endpoint)
+        raw_users = await resp.json()
+        return raw_users
 
     async def _create_user(self, params):
-        async with aiohttp.ClientSession() as session:
-            resp = await session.post(self._endpoint, json=params.dict())
-            raw_user = await resp.json()
-            return raw_user
+        resp = await self._session.post(self._endpoint, json=params.dict())
+        raw_user = await resp.json()
+        return raw_user
 
     def _convert_user(self, raw_user: Dict[str, Any]) -> User:
         return User(**raw_user)
 
 
-class UserRepositoryFactory:
-    def __init__(self):
-        self._repo = None
+async def get_session() -> aiohttp.ClientSession:
+    async with aiohttp.ClientSession() as session:
+        yield session
 
-    def __call__(self):
-        if self._repo is None:
-            self._repo = UserRepository()
-        return self._repo
+
+class UserRepositoryFactory:
+    async def __call__(self, session: aiohttp.ClientSession = Depends(
+        get_session)) -> UserRepository:
+        return UserRepository(session=session)
 
 
 get_user_repository = UserRepositoryFactory()
