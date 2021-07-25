a334833a43825c4cfebd7414ef9518b1c2d3355c
dnazymok <dnazymok@gmail.com>
2021-07-25 18:19:26 +0300

    fix tests

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -17,47 +17,50 @@ invalid_data = {
 }
 
 
-def test_post_list():
-    response = client.get("/posts")
-    assert response.status_code == 200
-    response_data = response.json()[0]
-    assert "id" in response_data
-    assert "title" in response_data
-    assert "body" in response_data
-    assert "author" in response_data
+class TestPostList:
+    def test_post_list(self):
+        response = client.get("/posts")
+        assert response.status_code == 200
+        response_data = response.json()[0]
+        assert "id" in response_data
+        assert "title" in response_data
+        assert "body" in response_data
+        assert "author" in response_data
 
 
-def test_post_create():
-    response = client.post("/posts/", json=valid_data)
-    assert response.status_code == 201
-    response_data = response.json()
-    assert response_data["id"] == 101
-    assert response_data["title"] == "Some Title"
-    assert response_data["body"] == "Some Body"
-    assert response_data["author"]["id"] == 1
+class TestPostCreate:
+    def test_post_create(self):
+        response = client.post("/posts/", json=valid_data)
+        assert response.status_code == 201
+        response_data = response.json()
+        assert response_data["id"] == 101
+        assert response_data["title"] == "Some Title"
+        assert response_data["body"] == "Some Body"
+        assert response_data["author"]["id"] == 1
+
+    def test_invalid_post_create(self):
+        response = client.post("/posts/", json=invalid_data)
+        assert response.status_code == 422
 
 
-def test_invalid_post_create():
-    response = client.post("/posts/", json=invalid_data)
-    assert response.status_code == 422
+class TestDetailPost:
+    def test_detail_post(self):
+        response = client.get("/posts/1")
+        assert response.status_code == 200
+        response_data = response.json()
+        assert "id" in response_data
+        assert "title" in response_data
+        assert "body" in response_data
+        assert "author" in response_data
+        assert "comments" in response_data
 
 
-def test_detail_post():
-    response = client.get("/posts/1")
-    assert response.status_code == 200
-    response_data = response.json()
-    assert "id" in response_data
-    assert "title" in response_data
-    assert "body" in response_data
-    assert "author" in response_data
-    assert "comments" in response_data
-
-
-def test_update_post():
-    valid_data = {"title": "string", "body": "string"}
-    response = client.patch("/posts/3", json=valid_data)
-    assert response.status_code == 200
-    response_data = response.json()
-    assert response_data["title"] == "string"
-    assert response_data["body"] == "string"
-    assert "id" in response_data
+class TestUpdatePost:
+    def test_update_post(self):
+        response = client.patch("/posts/3",
+                                json={"title": "string", "body": "string"})
+        assert response.status_code == 200
+        response_data = response.json()
+        assert response_data["title"] == "string"
+        assert response_data["body"] == "string"
+        assert "id" in response_data
--- tests/test_users.py
+++ tests/test_users.py
@@ -19,28 +19,29 @@ invalid_data = {
 }
 
 
-def test_user_list():
-    response = client.get("/users/")
-    assert response.status_code == 200
-    response_data = response.json()[0]
-    assert "id" in response_data
-    assert "name" in response_data
-    assert "username" in response_data
-    assert "email" in response_data
-    assert "phone" in response_data
+class TestUserList:
+    def test_user_list(self):
+        response = client.get("/users/")
+        assert response.status_code == 200
+        response_data = response.json()[0]
+        assert "id" in response_data
+        assert "name" in response_data
+        assert "username" in response_data
+        assert "email" in response_data
+        assert "phone" in response_data
 
 
-def test_create_user():
-    response = client.post("/users/", json=valid_data)
-    assert response.status_code == 201
-    response_data = response.json()
-    assert response_data["id"] == 11
-    assert response_data["name"] == "Geralt"
-    assert response_data["username"] == "Geralt"
-    assert response_data["email"] == "Geralt@example.com"
-    assert response_data["phone"] == "88005553535"
+class TestCreateUser:
+    def test_create_user(self):
+        response = client.post("/users/", json=valid_data)
+        assert response.status_code == 201
+        response_data = response.json()
+        assert response_data["id"] == 11
+        assert response_data["name"] == "Geralt"
+        assert response_data["username"] == "Geralt"
+        assert response_data["email"] == "Geralt@example.com"
+        assert response_data["phone"] == "88005553535"
 
-
-def test_invalid_user_create():
-    response = client.post("/users/", json=invalid_data)
-    assert response.status_code == 422
+    def test_invalid_user_create(self):
+        response = client.post("/users/", json=invalid_data)
+        assert response.status_code == 422
